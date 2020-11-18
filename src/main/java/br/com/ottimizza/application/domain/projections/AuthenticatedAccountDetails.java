0715b245cce4ad86bc5579901487bf1b919f8643
Lucas Martins <dev.lucasmartins@gmail.com>
2020-11-18 20:04:24 -0300

    tudo certo

---
+++ src/main/java/br/com/ottimizza/application/domain/projections/AuthenticatedAccountDetails.java
@@ -0,0 +1,15 @@
+package br.com.ottimizza.application.domain.projections;
+
+public interface AuthenticatedAccountDetails {
+
+    public String getAlias();
+
+    public String getUsername();
+
+    public String getAvatar();
+
+    public String getFirstName();
+
+    public String getLastName();
+
+}
