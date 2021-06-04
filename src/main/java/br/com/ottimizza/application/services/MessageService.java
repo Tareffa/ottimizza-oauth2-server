547fbd19b8410a73d9c6ec54e0e5e3ec373823ba
Daniel Steil <dani.steil@hotmail.com>
2021-06-04 16:12:30 -0300

    request para obter mensagem da variavel de ambiente

---
+++ src/main/java/br/com/ottimizza/application/services/MessageService.java
@@ -0,0 +1,22 @@
+package br.com.ottimizza.application.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import br.com.ottimizza.application.domain.MessageDTO;
+
+@Service
+public class MessageService {
+    
+    @Value("${message}")
+    private String MESSAGE_ALERT;
+
+    public MessageDTO getMessage() throws Exception {
+        System.out.println(MESSAGE_ALERT);
+        ObjectMapper mapper = new ObjectMapper();
+        MessageDTO message = mapper.readValue(MESSAGE_ALERT, MessageDTO.class);
+        return message;
+    }
+}
