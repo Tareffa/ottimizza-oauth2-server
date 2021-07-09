8cefd856b28937af26e31f8e321041bde1340b84
Daniel Steil <dani.steil@hotmail.com>
2021-07-09 10:42:22 -0300

    adicionando configuracaoMultipart

--- src/main/java/br/com/ottimizza/application/client/MailSenderClient.java
+++ src/main/java/br/com/ottimizza/application/client/MailSenderClient.java
@@ -1,15 +1,32 @@
 package br.com.ottimizza.application.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.ottimizza.application.domain.dtos.MailDTO;
 import br.com.ottimizza.application.domain.responses.GenericResponse;
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
 
-@FeignClient(name = "${email-sender.service.name}", url = "${email-sender.service.url}")
+@FeignClient(name = "${email-sender.service.name}", url = "${email-sender.service.url}", configuration = MailSenderClient.MultipartSupportConfig.class)
 public interface MailSenderClient {
+
+	@Configuration
+    public class MultipartSupportConfig {
+
+        @Bean
+        @Primary
+        @Scope("prototype")
+        public Encoder feignFormEncoder() {
+            return new SpringFormEncoder();
+        }
+    }
 
 	@PostMapping(value = "/api/v1/emails")
 	HttpEntity<GenericResponse<?>> sendMail(@RequestBody MailDTO mailDto);	
