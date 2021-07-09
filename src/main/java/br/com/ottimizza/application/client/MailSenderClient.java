4ca632c57db5464d4c740100fd907eae496ebe05
Daniel Steil <dani.steil@hotmail.com>
2021-07-09 11:07:16 -0300

    .

--- src/main/java/br/com/ottimizza/application/client/MailSenderClient.java
+++ src/main/java/br/com/ottimizza/application/client/MailSenderClient.java
@@ -1,18 +1,12 @@
 package br.com.ottimizza.application.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.ottimizza.application.domain.dtos.MailDTO;
 import br.com.ottimizza.application.domain.responses.GenericResponse;
-import feign.codec.Encoder;
-import feign.form.spring.SpringFormEncoder;
 
 @FeignClient(name = "${email-sender.service.name}", url = "${email-sender.service.url}")
 public interface MailSenderClient {
