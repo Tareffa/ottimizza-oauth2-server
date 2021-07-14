924c70b2689602f5884b95f5565fa0fc05594f47
Daniel Steil <dani.steil@hotmail.com>
2021-07-14 10:32:38 -0300

    removendo imports

--- src/main/java/br/com/ottimizza/application/client/MailSenderClient.java
+++ src/main/java/br/com/ottimizza/application/client/MailSenderClient.java
@@ -1,40 +1,19 @@
 package br.com.ottimizza.application.client;
 
-import org.springframework.beans.factory.ObjectFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.cloud.openfeign.support.SpringEncoder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import br.com.ottimizza.application.domain.dtos.MailDTO;
 import br.com.ottimizza.application.domain.responses.GenericResponse;
-import feign.Headers;
-import feign.codec.Encoder;
-import feign.form.spring.SpringFormEncoder;
 
-@FeignClient(name = "${email-sender.service.name}", url = "${email-sender.service.url}", configuration = MailSenderClient.MultipartSupportConfig.class)
+@FeignClient(name = "${email-sender.service.name}", url = "${email-sender.service.url}")
 public interface MailSenderClient {
 
-	@Configuration
-    public class MultipartSupportConfig {
+	
 
-        @Bean
-        @Primary
-        @Scope("prototype")
-        public Encoder feignFormEncoder() {
-            return new SpringFormEncoder();
-        }
-    }
-
-	@PostMapping(value = "/api/v1/emails", consumes = {"multipart/form-data"})
-    @Headers("Content-Type: multipart/form-data")
+	@PostMapping(value = "/api/v1/emails")
 	HttpEntity<GenericResponse<?>> sendMail(@RequestBody MailDTO mailDto);	
 
 }
